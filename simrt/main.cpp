07bf8e9f967a3d6530ad3fce717a71a7f382cf23
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-02-02 17:49:04 -0500

    Display events
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- simrt/main.cpp
+++ simrt/main.cpp
@@ -1,13 +1,33 @@
 #include <QDebug>
+#include <iostream>
 
 #include "perf.h"
+
+void display_events()
+{
+    PerfEvent perf;
+
+    // hum... this is quite convoluted just to display a sorted list
+
+    QSet<QString> set = perf.availableEvents();
+    QVector<QString> events;
+    for (const QString name: set) {
+        events.append(name);
+    }
+    qSort(events);
+
+    std::cout << events.size() << " events available(s):" << std::endl;
+    for (const QString name: events) {
+        std::cout << "    " << name.toStdString() << std::endl;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     Q_UNUSED(argc);
     Q_UNUSED(argv);
 
-    qDebug() << "hello";
+    display_events();
 
     return 0;
 }
