91c05aca73ef54a9d43eaef7b8286cc3d8cdca93
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-02-08 12:39:18 -0500

    LTTng snapshot example
    The LTTng API allows to take a snapshot of the trace buffer. The snapshot
    function is synchronous. The snapshot is triggered in a background thread, and
    no more than one snapshot is active at a time.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- test/traceapi/main.cpp
+++ test/traceapi/main.cpp
@@ -1,9 +1,13 @@
 #include <QDebug>
 #include <QThread>
+#include <QRunnable>
+#include <QThreadPool>
+#include <QMutex>
 
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <functional>
 #include <lttng/lttng.h>
 
 void dump(char *data, int size) {
@@ -13,15 +17,53 @@ void dump(char *data, int size) {
     std::cout << std::endl;
 }
 
+QMutex mutex;
+bool active;
+
+class LambdaRunnable : public QRunnable
+{
+public:
+    LambdaRunnable(std::function<void ()> fn) : m_fn(fn) { }
+    void run() {
+        m_fn();
+    }
+private:
+    std::function<void ()> m_fn;
+};
+
+void trigger_snapshot()
+{
+    mutex.lock();
+    if (active) {
+        mutex.unlock();
+        return;
+    }
+    active = true;
+    QRunnable *snap = new LambdaRunnable([](){
+        lttng_snapshot_output *snap = lttng_snapshot_output_create();
+        int ret = lttng_snapshot_record("foo", snap, 0);
+        qDebug() << ret;
+        mutex.lock();
+        active = false;
+        mutex.unlock();
+    });
+    QThreadPool::globalInstance()->start(snap);
+    mutex.unlock();
+}
+
 int main(int argc, char *argv[])
 {
     (void) argc; (void) argv;
     int ret;
 
+    QThreadPool::globalInstance()->start(new LambdaRunnable([](){
+        return;
+    }));
+
     lttng_domain domain = { };
     domain.type = LTTNG_DOMAIN_KERNEL;
 
-    ret = lttng_create_session("foo", "file:///tmp/foo");
+    ret = lttng_create_session_snapshot("foo", "file:///tmp/foo");
     qDebug() << ret;
 
     lttng_handle *handle = lttng_create_handle("foo", &domain);
@@ -56,9 +98,16 @@ int main(int argc, char *argv[])
     }
     free(events); events = nullptr;
 
+    // TODO: how to enable all tracepoints/syscalls/ust?
     lttng_event ev = { };
-    strcpy(ev.name, "sched_switch");
+    strncpy(ev.name, "sched_switch", LTTNG_SYMBOL_NAME_LEN);
     ev.type = LTTNG_EVENT_TRACEPOINT;
+    ev.loglevel_type = LTTNG_EVENT_LOGLEVEL_ALL;
+    ret = lttng_enable_event(handle, &ev, "k");
+    qDebug() << ret;
+
+    strcpy(ev.name, "open");
+    ev.type = LTTNG_EVENT_SYSCALL;
     ev.loglevel_type = LTTNG_EVENT_LOGLEVEL_ALL;
     ret = lttng_enable_event(handle, &ev, "k");
     qDebug() << ret;
@@ -66,7 +115,13 @@ int main(int argc, char *argv[])
     ret = lttng_start_tracing("foo");
     qDebug() << ret;
 
-    QThread::msleep(1);
+    for (int i = 0; i < 3; i++) {
+        trigger_snapshot();
+        QThread::msleep(1);
+    }
+
+    QThreadPool::globalInstance()->waitForDone();
+
     ret = lttng_stop_tracing("foo");
     qDebug() << ret;
 
